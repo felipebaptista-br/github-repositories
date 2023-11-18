4b4f96c649a1bc07e6eb20b624d18e2353ea42a0
Felipe Baptista <felipe.baptista06@gmail.com>
2023-11-18 12:24:18 -0300

    new authenticate in layout.js - dashboard

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,9 +1,14 @@
+'use client'
 
+import { useRouter } from "next/navigation"
+import { authLogin } from "@/utils/authentication/auth"
 import Header from "@/components/layout/header"
 import Footer from "@/components/layout/footer"
 
 export default function RootLayout({ children }) {
-  
+  const router = useRouter()
+
+  if (authLogin()) {
     return (
       <div>
         <Header />
@@ -11,4 +16,7 @@ export default function RootLayout({ children }) {
         <Footer />
       </div>
     )
+  } else {
+    router.push('/')
+  }
 }
